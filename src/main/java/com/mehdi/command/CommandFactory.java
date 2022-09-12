2706328d17395688eed213dd0d556254941c2346
nikfarjam <mm.nikfarjam@gmail.com>
2022-09-12 18:20:21 +1000

    Implement command

--- src/main/java/com/mehdi/command/CommandFactory.java
+++ src/main/java/com/mehdi/command/CommandFactory.java
@@ -1,71 +1,80 @@
 package com.mehdi.command;
 
 import com.mehdi.board.Board;
-import com.mehdi.board.BoardImpl;
 import com.mehdi.error.InvalidCommand;
+import com.mehdi.model.Direction;
 import com.mehdi.model.Position;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CommandFactory {
-
-    private static CommandFactory instance;
-
     private Board board;
+    private Pattern placeCommand;
+    private Pattern robotCommand;
 
-    private CommandFactory() {
-    }
-
-    public static CommandFactory getInstance() {
-        if (instance == null) {
-            //synchronized block to remove overhead
-            synchronized (CommandFactory.class) {
-                if (instance == null) {
-                    // if instance is null, initialize
-                    instance = new CommandFactory();
-                }
-            }
-        }
-        return instance;
+    public void setBoard(Board board) {
+        this.board = board;
     }
 
     public Board getBoard() {
-        if (board == null) {
-            board = new BoardImpl(5, 5);
-        }
-        return board;
+        return this.board;
     }
 
-    public Command createCommand(String cmd) {
-        Board localBoard = getBoard();
+    public Pattern getPlaceCommand() {
+        if (placeCommand == null) {
+            placeCommand = Pattern.compile("^PLACE\\s+(\\d)\\s*,\\s*(\\d)\\s*,\\s*(NORTH|EAST|SOUTH|WEST)");
+        }
+        return placeCommand;
+    }
 
-        sanitationCommandString();
-        validateCommandString();
-        cmd = cmd.toUpperCase();
+    public Pattern getRobotCommand() {
+        if (robotCommand == null) {
+            robotCommand = Pattern.compile("^ROBOT\\s+(\\d)");
+        }
+        return robotCommand;
+    }
 
-        if (cmd.startsWith("PLACE")) {
-            return new Place(localBoard, Position.Builder.aPosition().build());
+    public Command createCommand(String command) {
+        sanitationCommandString(command);
+        validateCommandString(command);
+        var cmd = command.toUpperCase();
+
+        Matcher placeCommandMatcher = getPlaceCommand().matcher(cmd);
+        if (placeCommandMatcher.find()) {
+            int column = Integer.parseInt(placeCommandMatcher.group(1));
+            int row = Integer.parseInt(placeCommandMatcher.group(2));
+            Direction facing = Direction.valueOf(placeCommandMatcher.group(3));
+            Position p = Position.Builder.aPosition().column(column).row(row).facing(facing).build();
+            return new Place(this.board, p);
         }
         if (cmd.equals("LEFT")) {
-            return new Left(localBoard);
+            return new Left(this.board);
         }
         if (cmd.equals("RIGHT")) {
-            return new Right(localBoard);
+            return new Right(this.board);
         }
-        if (cmd.startsWith("ROBOT")) {
-            return new Active(localBoard, 0);
+        Matcher robotMatcher = getRobotCommand().matcher(cmd);
+        if (robotMatcher.find()) {
+            int index = Integer.parseInt(robotMatcher.group(1));
+            return new Active(this.board, index);
         }
         if (cmd.equals("MOVE")) {
-            return new Move(localBoard);
+            return new Move(this.board);
         }
         if (cmd.startsWith("REPORT")) {
-            return new Report(localBoard);
+            return new Report(this.board);
         }
         throw new InvalidCommand("Command is not in valid list");
     }
 
-    private void validateCommandString() throws InvalidCommand {
+    private void validateCommandString(String cmd) throws InvalidCommand {
+        if (cmd == null || "".equals(cmd.trim())) {
+            throw new InvalidCommand("Command is empty");
+        }
     }
 
-    private void sanitationCommandString() throws InvalidCommand {
-
+    private void sanitationCommandString(String cmd) throws InvalidCommand {
+        // Use regular exprestion to check if a command contains harmful script
     }
 }
